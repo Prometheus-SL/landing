19c53a2b752b453dc1e54af1622e8d225a5da158
Miguel Angel Perez Garcia <miguelprozlol@gmail.com>
2025-09-29 22:41:53 +0200

    feat: add theme toggle component and dropdown menu
    - Implemented ModeToggle component for switching between light, dark, and system themes.
    - Created DropdownMenu component with various items and functionalities.
    - Added ThemeProvider to manage theme state and persist it in local storage.
    - Integrated lucide-react icons for visual representation in the theme toggle.

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,23 @@
 import { useState } from "react";
 import { Button } from "@/components/ui/button";
 import { AppleHelloEnglishEffect } from "@/components/ui/shadcn-io/apple-hello-effect";
+import { ModeToggle } from "./components/mode-toggle";
 
 const App = () => {
   const [count, setCount] = useState(0);
   return (
-    <div className="flex w-full h-screen flex-col justify-center items-center gap-16">
-      <AppleHelloEnglishEffect speed={1.1} />
-      <Button onClick={() => setCount((count) => count + 1)}>
-        count is {count}
-      </Button>
-    </div>
+    <>
+      <div className="flex w-full h-screen flex-col justify-center items-center gap-16">
+        <AppleHelloEnglishEffect speed={1.1} />
+        <Button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </Button>
+        <ModeToggle />
+      </div>
+      <div className="fixed bottom-4 right-4 opacity-50 hover:opacity-100 transition">
+        <ModeToggle />
+      </div>
+    </>
   );
 };
 export default App;
