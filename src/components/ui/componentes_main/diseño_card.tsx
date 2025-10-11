f8901aad63d856a219eefdfc6b7b07bc6c64d233
Rauke10 <67696106+Rauke10@users.noreply.github.com>
2025-10-11 16:14:05 +0200

    feat: eliminar margen superior en el contenedor de AboutUs en Landing_Card

--- src/components/ui/componentes_main/diseño_card.tsx
+++ src/components/ui/componentes_main/diseño_card.tsx
@@ -20,7 +20,7 @@ export function Landing_Card() {
           </CardBody>
         </CardContainer>
     </div>
-    <div className="flex justify-center mt-10">
+    <div className="flex justify-center">
       <AboutUs />
     </div>
     </>
